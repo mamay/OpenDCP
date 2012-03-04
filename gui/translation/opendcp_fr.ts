eff1230b6fc2db6494fe968753a7238a9357c35c
Terrence Meiczinger <terrence72@gmail.com>
2012-03-03 19:13:28 -0500

    adding translation files

---
+++ gui/translation/opendcp_fr.ts
@@ -0,0 +1,399 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="fr_FR">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../j2k.cpp" line="57"/>
+        <location filename="../j2k.cpp" line="58"/>
+        <location filename="../mxf.cpp" line="147"/>
+        <source>Left:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../j2k.cpp" line="68"/>
+        <location filename="../j2k.cpp" line="69"/>
+        <location filename="../mxf.cpp" line="153"/>
+        <source>Directory:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../j2k.cpp" line="207"/>
+        <location filename="../j2k.cpp" line="235"/>
+        <source>No image files found</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../j2k.cpp" line="208"/>
+        <location filename="../j2k.cpp" line="236"/>
+        <source>No valid image files were found in the selected directory</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../j2k.cpp" line="304"/>
+        <source>Source Directories Needed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../j2k.cpp" line="304"/>
+        <source>Please select source directories</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../j2k.cpp" line="307"/>
+        <source>Destination Directories Needed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../j2k.cpp" line="307"/>
+        <source>Please select destination directories</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../j2k.cpp" line="314"/>
+        <source>Source Directory Needed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../j2k.cpp" line="314"/>
+        <source>Please select a source directory</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../j2k.cpp" line="317"/>
+        <source>Destination Directory Needed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../j2k.cpp" line="317"/>
+        <source>Please select a destination directory</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../j2k.cpp" line="325"/>
+        <location filename="../mxf.cpp" line="409"/>
+        <source>File Count Mismatch</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../j2k.cpp" line="326"/>
+        <location filename="../mxf.cpp" line="410"/>
+        <source>The left and right image directories have different file counts. They must be the same. Please fix and try again.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../j2k.cpp" line="334"/>
+        <source>Invalid DCI Resolution</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../j2k.cpp" line="335"/>
+        <source>Images are not DCI compliant, select DCI resize to automatically resize or supply DCI compliant images</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="94"/>
+        <source>E&amp;xit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="94"/>
+        <source>Ctrl+Q</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="98"/>
+        <source>About OpenDCP</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="111"/>
+        <source>Choose an mpeg2 file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="113"/>
+        <source>Choose a directory of images</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="117"/>
+        <source>DCDM Subtitle File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="120"/>
+        <source>Save MXF as</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="123"/>
+        <location filename="../xml.cpp" line="255"/>
+        <location filename="../xml.cpp" line="286"/>
+        <source>Choose a file to open</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="188"/>
+        <source>File Sequence Check Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="188"/>
+        <source>All filenames must be of the same length.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="195"/>
+        <source>File Sequence Mismatch</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mxf.cpp" line="155"/>
+        <source>M2V File:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mxf.cpp" line="187"/>
+        <location filename="../mxf.cpp" line="213"/>
+        <location filename="../mxf.cpp" line="225"/>
+        <location filename="../mxf.cpp" line="267"/>
+        <source>Destination file needed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mxf.cpp" line="187"/>
+        <location filename="../mxf.cpp" line="213"/>
+        <source>Please select a destination picture MXF file.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mxf.cpp" line="192"/>
+        <location filename="../mxf.cpp" line="197"/>
+        <location filename="../mxf.cpp" line="217"/>
+        <location filename="../mxf.cpp" line="231"/>
+        <location filename="../mxf.cpp" line="240"/>
+        <source>Source content needed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mxf.cpp" line="192"/>
+        <source>Please select left and right source image directories to generate a stereoscopic MXF file.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mxf.cpp" line="197"/>
+        <source>Please select a source image directory to generate an MXF picture file.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mxf.cpp" line="204"/>
+        <source>Invalid frame rate</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mxf.cpp" line="204"/>
+        <source>Stereoscopic MXF only supports 24, 25, or 30 fps.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mxf.cpp" line="217"/>
+        <source>Please select a source MPEG2 MXF file.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mxf.cpp" line="225"/>
+        <source>Please select a destination sound MXF file.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mxf.cpp" line="231"/>
+        <source>Please specify left and right wav files to generate an MXF sound file.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mxf.cpp" line="240"/>
+        <source>Please specify at least 5.1 wav files to generate an MXF sound file.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mxf.cpp" line="262"/>
+        <source>Source subtitle needed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mxf.cpp" line="262"/>
+        <source>Please specify an input subtitle XML file.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mxf.cpp" line="267"/>
+        <source>Please specify the destinaton subtitle MXF file.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mxf.cpp" line="286"/>
+        <location filename="../mxf.cpp" line="289"/>
+        <location filename="../mxf.cpp" line="354"/>
+        <location filename="../mxf.cpp" line="432"/>
+        <location filename="../mxf.cpp" line="440"/>
+        <source>MXF Creation Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mxf.cpp" line="287"/>
+        <location filename="../mxf.cpp" line="290"/>
+        <source>Subtitle MXF creation failed.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mxf.cpp" line="355"/>
+        <source>Sound MXF creation failed.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mxf.cpp" line="432"/>
+        <source>No input files found</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mxf.cpp" line="440"/>
+        <source>Picture MXF creation failed.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../xml.cpp" line="82"/>
+        <source>Missing Picture Track</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../xml.cpp" line="83"/>
+        <source>An MXF picture track is required</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../xml.cpp" line="90"/>
+        <source>Duration Mismatch</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../xml.cpp" line="91"/>
+        <source>The duration of all MXF tracks must be the same.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../xml.cpp" line="117"/>
+        <source>Add Reel Failed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../xml.cpp" line="118"/>
+        <source>Could not add reel to CPL.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../xml.cpp" line="131"/>
+        <source>Validating Reel Failed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../xml.cpp" line="132"/>
+        <source>Could not valiate reel.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../xml.cpp" line="137"/>
+        <source>Choose destination folder</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../xml.cpp" line="160"/>
+        <location filename="../xml.cpp" line="175"/>
+        <source>Write CPL Failed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../xml.cpp" line="161"/>
+        <location filename="../xml.cpp" line="176"/>
+        <source>Failed to create CPL.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../xml.cpp" line="165"/>
+        <source>Write PKL Failed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../xml.cpp" line="166"/>
+        <source>Failed to create PKL.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../xml.cpp" line="170"/>
+        <source>Write VOLNDEX Failed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../xml.cpp" line="171"/>
+        <source>Failed to create VOLINDEX.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../xml.cpp" line="186"/>
+        <location filename="../xml.cpp" line="199"/>
+        <location filename="../xml.cpp" line="212"/>
+        <source>Move MXF File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../xml.cpp" line="186"/>
+        <source>The destination DCP folder and source picture MXF folder are different. Do you want move (not copy) the MXF file to the destination DCP folder?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../xml.cpp" line="199"/>
+        <source>The destination DCP folder and source sound MXF folder are different. Do you want move (not copy) the MXF file to the destination DCP folder?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../xml.cpp" line="212"/>
+        <source>The destination DCP folder and source subtitle MXF folder are different. Do you want move (not copy) the MXF file to the destination DCP folder?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../xml.cpp" line="264"/>
+        <source>Not a Picture Track</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../xml.cpp" line="265"/>
+        <source>The selected file is not a valid MXF picture track.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../xml.cpp" line="295"/>
+        <source>Not a Sound Track</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../xml.cpp" line="296"/>
+        <source>The selected file is not a valid MXF sound track.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../xml.cpp" line="317"/>
+        <source>Choose an file to open</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../xml.cpp" line="326"/>
+        <source>Not a Subtitle Track</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../xml.cpp" line="327"/>
+        <source>The selected file is not a valid MXF subtitle track.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
